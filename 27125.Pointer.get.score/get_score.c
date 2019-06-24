9e6ee0186160c5e3e75ef7b12e0a0f736550d295
TaeKwan Jung <50404450+TKJung@users.noreply.github.com>
2019-06-24 21:31:24 +0900

    Create get_score.c

---
+++ 27125.Pointer.get.score/get_score.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+
+int getSize( int *pStu, int *pCourse )
+{
+	int numStu2; // 입력받는 학생 수
+	int numCourse2; // 입력받는 과목 수
+
+	printf("학생 수를 입력하세요: ");
+	scanf("%d", &numStu2 );  
+	
+	printf("과목 수를 입력하세요: ");
+	scanf("%d", &numCourse2 );
+
+
+	// 학생수= 1 ~ 100명, 과목수= 1 ~ 10명
+	if (  (1 <= numStu2 && numStu2 <= 100) && ( 1 <= numCourse2 && numCourse2 <= 10 ) ) 
+	{
+		*pStu = numStu2;
+		*pCourse = numCourse2; 
+		return 1;
+	}
+
+	else if (  (numStu2 < -2 || numStu2 == 0 || numStu2 > 100) // 학생수= 0이하 or 100초과
+			   || (numCourse2 < -2 || numCourse2 == 0 || numCourse2 > 10)  ) // 과목수=0이하 or 10초과
+	{
+		printf("틀렸습니다.\n다시 학생수를 입력하세요: ");
+		scanf("%d", &numStu2 );  
+		
+		printf("과목 수를 입력하세요: ");
+		scanf("%d", &numCourse2 );
+		
+		*pStu = numStu2;
+		*pCourse = numCourse2; 
+		
+		return 1;
+	}
+
+	else if ( numStu2 == EOF || numCourse2 == EOF ) // 학생수=EOF가 되면 0을 돌려줌으로써 종료
+	{
+		return 0;
+	}
+
+	else  // 학생수 = 숫자가 아닌 문자
+	{
+		printf("틀렸습니다.\n다시 학생수를 입력하세요: ");
+		scanf("%d", &numStu2 );  
+		
+		printf("과목 수를 입력하세요: ");
+		scanf("%d", &numCourse2 );
+		
+		*pStu = numStu2;
+		*pCourse = numCourse2; 
+		
+		return 1;
+	}
+}
+
+int main(int ac, char *av[])
+{
+	int numStu = 0;  // 학생 수
+	int numCourse = 0; // 과목 수
+
+	if (  getSize( &numStu, &numCourse )  == 1  )
+	{
+		printf("학생 수: %d, 과목 수: %d\n", numStu, numCourse);
+	}
+
+	else
+	{
+		printf("실패\n");
+	}
+
+}
