ed104299c8c3bbc49c33a53e3cd5cbe5dc666faa
TaeKwan Jung <50404450+TKJung@users.noreply.github.com>
2019-09-11 00:05:46 +0900

    Update get_score.c

--- 27125.Pointer.get.score/get_score.c
+++ 27125.Pointer.get.score/get_score.c
@@ -2,8 +2,8 @@
 
 int getSize( int *pStu, int *pCourse )
 {
-	int numStu2; // 입력받는 학생 수
-	int numCourse2; // 입력받는 과목 수
+	int numStu2 = -1; // 입력받는 학생 수
+	int numCourse2 = -1; // 입력받는 과목 수
 
 	printf("학생 수를 입력하세요: ");
 	scanf("%d", &numStu2 );  
@@ -20,8 +20,8 @@ int getSize( int *pStu, int *pCourse )
 		return 1;
 	}
 
-	else if (  (numStu2 < -2 || numStu2 > 100) // 학생수= 0이하 or 100초과
-			   || (numCourse2 < -2 || numCourse2 > 10)  ) // 과목수=0이하 or 10초과
+	else if (  (numStu2 < -2 || numStu2 == 0 || numStu2 > 100) // 학생수= 0이하 or 100초과
+			   || (numCourse2 < -2 || numCourse2 == 0 || numCourse2 > 10)  ) // 과목수=0이하 or 10초과
 	{
 		getSize(&numStu2, &numCourse2);
 		
@@ -35,12 +35,7 @@ int getSize( int *pStu, int *pCourse )
 		return 0;
 	}
 	
-	// 학생수=숫자가 아닌 문자가 되면, 0을 돌려줌으로써 종료
-	else if (numStu2 == 0 || numCourse2 == 0 ) 
-	{
-		return 0;
-	}
-	else 
+	else // 학생수=숫자가 아닌 문자가 되면, 0을 돌려줌으로써 종료
 	{
 		return 0;
 	}
